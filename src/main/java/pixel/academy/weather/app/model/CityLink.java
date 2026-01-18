e9aab4e529c36a3b6c348329588f78d54856d69b
Ion Schimbator <ionschimbator98@gmail.com>
2026-01-18 17:45:47 +0200

    feat(model): add City enum, WeatherResponse DTO and CityLink DTO for href links

---
+++ src/main/java/pixel/academy/weather/app/model/CityLink.java
@@ -0,0 +1,32 @@
+package pixel.academy.weather.app.model;
+
+public class CityLink {
+
+    private String name;
+    private String href;
+
+    public CityLink() {
+
+    }
+
+    public CityLink(String name, String href) {
+        this.name = name;
+        this.href = href;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getHref() {
+        return href;
+    }
+
+    public void setHref(String href) {
+        this.href = href;
+    }
+}

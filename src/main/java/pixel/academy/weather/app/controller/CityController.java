c481539b8458b24e62e0f977ffe5e5b52d8685dd
Ion Schimbator <ionschimbator98@gmail.com>
2026-01-20 20:10:04 +0200

    refactor: remove CityLink - not needed with HTML frontend

--- src/main/java/pixel/academy/weather/app/controller/CityController.java
+++ src/main/java/pixel/academy/weather/app/controller/CityController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pixel.academy.weather.app.model.City;
-import pixel.academy.weather.app.model.CityLink;
 
 import java.util.Arrays;
 import java.util.List;
@@ -16,13 +15,9 @@ import java.util.stream.Collectors;
 public class CityController {
 
     @GetMapping
-    public List<CityLink> getCities(HttpServletRequest request) {
-
+    public List<String> getCities() {
         return Arrays.stream(City.values())
-                .map(city -> new CityLink(
-                        city.getName(),
-                        "/weather/" + city.getName()
-                ))
+                .map(City::getName)
                 .collect(Collectors.toList());
     }
 }

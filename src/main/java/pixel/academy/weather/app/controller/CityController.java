ebe2ba54308dbf0859d878b6fc789641ef67efbd
Ion Schimbator <ionschimbator98@gmail.com>
2026-01-27 17:35:22 +0200

    refactor: migrate HTML pages to Thymeleaf templates

--- src/main/java/pixel/academy/weather/app/controller/CityController.java
+++ src/main/java/pixel/academy/weather/app/controller/CityController.java
@@ -1,6 +1,5 @@
 package pixel.academy.weather.app.controller;
 
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +10,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/cities")
+@RequestMapping("/api/cities")
 public class CityController {
 
     @GetMapping
